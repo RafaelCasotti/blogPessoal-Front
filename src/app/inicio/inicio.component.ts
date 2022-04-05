f3f62834f9ed3217307771791cd08255c8ad4100
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-04-05 15:27:30 -0300

    @RafaelCasotti [Task:Implementação do Post e Get de postagem]

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,13 @@
+import { CoreEnvironment } from '@angular/compiler/src/compiler_facade_interface';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +16,26 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+
+  listaTemas: Tema[]
+
+  listaPostagens: Postagem[]
+
+  idTema: number
+
+  tema: Tema = new Tema()
+
+  usuario: Usuario = new Usuario()
+
+  idUsuario = environment.id
+
   constructor(
-private router : Router
+private router : Router,
+private postagemService: PostagemService,
+private temaService: TemaService,
+private authService: AuthService
+
 
   ) { }
 
@@ -19,6 +44,50 @@ private router : Router
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+
+    this.getAllTemas()
+    this.getAllPostagens()
+    this.authService.refreshToken()
+
+  }
+
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) =>{
+      this.listaPostagens = resp
+    })
+  }
+
+  findByIdUser(){
+    this.authService.getByIdUser(this.idUsuario).subscribe((resp: Usuario) =>{
+      this.usuario = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) =>{
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -11,16 +11,30 @@ import { Usuario } from '../model/Usuario';
 })
 export class AuthService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token),
+    }
+  }
 
     entrar(userLogin: UserLogin): Observable<UserLogin>{
       return this.http.post<UserLogin>('https://blogpessoalrafaelcasotti.herokuapp.com/usuarios/logar', userLogin)
     }
+
     cadastrar( usuario: Usuario) : Observable<Usuario>{
       return this.http.post<Usuario>('https://blogpessoalrafaelcasotti.herokuapp.com/usuarios/cadastrar', usuario)
     }
+
+    getByIdUser(id: number): Observable<Usuario>{
+      return this.http.get<Usuario>(`https://blogpessoalrafaelcasotti.herokuapp.com/usuarios/${id}` , this.token)
+    }
+
     logado(){
       let ok : boolean = false
 
