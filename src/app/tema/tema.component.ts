670aa6cf9b8e838dfc686bf32e3abbf527f42c3f
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-04-08 20:31:18 -0300

    @RafaelCasotti [Task: Adicionando a opção administrador]

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
+import { AlertasService } from '../service/alertas.service';
 import { TemaService } from '../service/tema.service';
 
 @Component({
@@ -16,13 +17,18 @@ export class TemaComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alertaService: AlertasService
   ) { }
 
   ngOnInit() {
 
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
+    }
+    if(environment.tipo != 'adm'){
+      this.alertaService.showAlertInfo('Você precisa ser administrador para acessar essa rota.')
+      this.router.navigate(['/inicio'])
     }
 
     this.findAllTemas()
@@ -38,7 +44,7 @@ export class TemaComponent implements OnInit {
   cadastrar(){
     this.temaService.postTema(this.tema).subscribe((resp: Tema) =>{
       this.tema = resp
-      alert('Tema cadastrado com sucesso! ✔')
+      this.alertaService.showAlertSuccess('Tema cadastrado com sucesso! ✔')
       this.findAllTemas()
       this.tema = new Tema()
     })
